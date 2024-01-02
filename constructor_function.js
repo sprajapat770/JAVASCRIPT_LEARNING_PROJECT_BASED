db5766ac2791a4d4dc8462cbdd4a69c410b54262
Suraj Prajapat <sprajapat770@gmail.com>
2024-01-02 11:43:21 +0530

    learning constructor function

---
+++ constructor_function.js
@@ -0,0 +1,36 @@
+function BankAccount(customerName, balance) {
+    this.customerName = customerName;
+    this.accountNumber = Date.now();
+    this.balance = balance;
+    this.deposit = function (deposit) {
+        this.balance += deposit;
+    }
+
+    this.withdraw = (withdraw) => { 
+        this.balance -= withdraw;
+    }
+}
+
+const accounts = [];
+
+const accountForm = document.getElementById('accountForm');
+const customerName = document.getElementById('customerName');
+const balance = document.getElementById('balance');
+
+accountForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const account = new BankAccount(customerName.value, +balance.value);
+    accounts.push(account);
+    console.log(accounts);
+});
+
+const depositeForm = document.getElementById('depositeForm');
+const accountNumber = document.getElementById('accountNumber');
+const amount = document.getElementById('amount');
+
+depositeForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const account = accounts.find(account => account.accountNumber === +accountNumber.value);
+    account.deposit(+amount.value);
+    console.log(account);
+});
